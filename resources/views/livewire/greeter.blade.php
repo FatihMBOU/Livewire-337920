c1fa75997d45ab7f06450de4e9cb82dde3ec6623
Fatih Kuzu <337920@student.mboutrecht.nl>
2025-01-23 09:34:59 +0100

    De hebben paar extra code verwijderd waar ik mijn probleem heb opgelost, vandaag en morgen ga ik werken aan deze opdracht te afronden na drukke project tijden. VIDEO 4 is klaar

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,42 +1,41 @@
 <div>
-    <div>
-        {{$greeting}}, {{$name}}!
-    </div>
-    <form 
-    wire:submit="changeName()" 
+    <form
+        wire:submit="changeGreeting()"
     >
         <div class="mt-2">
-            <select
-                type="text" 
-                class="p-4 border-rounded-md bg-gray-700 text-white"
-                wire:model.fill="greeting"
+            <select 
+                    type="text" 
+                    class="p-4 border rounded-md bg-gray-700 text-white"
+                    wire:model.fill="greeting"
                 >
-                
-            <option value="Hello">Hello</option>
-                <option value="Hi">Hi</option>
-                <option value="Hey">Hey</option>
-                <option value="Hiya">Hiya</option>
-                
+                    <option value="Hello">Hello</option>
+                    <option value="Hi">Hi</option>
+                    <option value="Hey">Hey</option>
+                    <option value="Howdy" selected>Howdy</option>
             </select>
-            <input
+            <input 
                 type="text" 
-                class="p-4 border-rounded-md bg-gray-700 text-white"
+                class="p-4 border rounded-md bg-gray-700 text-white"
                 wire:model="name"
-                >
+            >
         </div>
-
+        <div>
+            @error('name')
+            {{$message}}
+            @enderror
+        </div>
         <div class="mt-2">
-            <button
-            type="submit"
-            class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
+            <button 
+                type="submit"
+                class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
             >
                 Greet
             </button>
         </div>
     </form>
-    @if ($name == '')
-    <div class="mt-5">
-        {{$greeting}}, {{$name}}!
+    @if ($greetingMessage !== '')
+    <div>
+        {{$greetingMessage}}
     </div>
     @endif
 </div>
