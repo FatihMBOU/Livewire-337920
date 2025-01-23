c5cc801f7cd4441d21a003ecb867eacb8534f545
Fatih Kuzu <337920@student.mboutrecht.nl>
2025-01-23 10:55:57 +0100

    Les 7 klaar, de vorige commit is in video 5 gedaan maar dat was les 6, dus perongeluk 5 en 6 in 5 gedaan daarom dat dit 7 heet, VIDEO 7 is klaar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
+
 <?php
 
+use App\Livewire\Search;
+use App\Livewire\ShowArticle;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/search', Search::class);
+Route::get('/articles/{article}', ShowArticle::class);
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
