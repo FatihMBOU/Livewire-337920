b59262a08ba691c467cc95ef79ca9fca24a81385
Fatih Kuzu <337920@student.mboutrecht.nl>
2025-01-24 11:10:27 +0100

    VIDEO 13 is klaar, vandaag is laatste dag, ik ga proberen de 30 te halen. uiteindelijk is mijn project ook klaar dus kan ik volle focus hierop

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    /** @use HasFactory<\Database\Factories\ArticleFactory> */
     use HasFactory;
+
+    protected $fillable = ['title', 'content'];
 }
