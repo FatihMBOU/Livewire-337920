76b56b21573efd680be519eb87d0fda2803f9782
Fatih Kuzu <337920@student.mboutrecht.nl>
2025-01-24 11:24:42 +0100

    LES 14 is klaar, we waren bezgi net als vorige opdracht met edit en create, alleen hebben we nog notificatie opties toegevoegd

--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -5,7 +5,7 @@
             <label class="block" for="article-title">Title</label>
             <input 
                 type="text" 
-                class="p-2 w-full border rounded-md bg-gray-700 text-white"
+                class="p-2 w-full border rounded-md bg-gray-700 text-black"
                 wire:model="form.title"
             >
             <div>
@@ -16,11 +16,45 @@
             <label class="block" for="article-content">Content</label>
             <textarea 
                 id="article-content""
-                class="p-2 w-full border rounded-md bg-gray-700 text-white"
+                class="p-2 w-full border rounded-md bg-gray-700 text-black"
                 wire:model="form.content"
             ></textarea>
             <div>
                 @error('content') <span class="text-red-600">{{ $message }}</span> @enderror
+            </div>
+        </div>
+        <div class="mb-3">
+            <label class="flex items-center">
+                <input type="checkbox" name="published" 
+                    class="mr-2"
+                    wire:model.boolean="form.published"
+                >
+                Published
+            </label>
+        </div>
+        <div class="mb-3">
+            <div>
+                <div class="mb-2">Notification Options</div>
+                <div class="flex gap-6">
+                    <label class="flex items-center">
+                        <input type="radio" value="email" class="mr-2"
+                            wire:model="form.notification"
+                        >
+                        Email
+                    </label>
+                    <label class="flex items-center">
+                        <input type="radio" value="sms" class="mr-2"
+                            wire:model="form.notification"
+                        >
+                        SMS
+                    </label>
+                    <label class="flex items-center">
+                        <input type="radio" value="none" class="mr-2"
+                            wire:model="form.notification"
+                        >
+                        None
+                    </label>
+                </div>
             </div>
         </div>
         <div class="mb-3">
